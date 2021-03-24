cc061c7b66d189fe6147ffbd78f3d1629e2b784d
qucumbah <sc.buher@yandex.ru>
2021-03-24 19:53:16 +0300

    remove old code

--- src/main.rs
+++ src/main.rs
@@ -45,26 +45,3 @@ fn test_agent(
         output_file.write(b"\n").expect("Should have written the output");
     }
 }
-
-// fn test_agent(agent: &mut dyn crate::agents::Agent, description: &str) {
-//     let n_steps = 5000;
-//     environment::run(agent, n_steps);
-//     println!("{}", description);
-//     log_error(&agent.get_expected_rewards());
-// }
-
-// fn log_error(expected_rewards: &[f32; 10]) {
-//     let mut total_error = 0.0;
-//     let actual_rewards: [f32; 10] = environment::get_rewards();
-//     println!("{:>7} {:>20} {:>20} {:>20}", "Action", "Actual reward", "Expected reward", "Error");
-//     for i in 0..10 {
-//         let expected_reward = expected_rewards[i];
-//         let actual_reward = actual_rewards[i];
-//         let error = (expected_reward - actual_reward).powf(2.0);
-
-//         println!("{:>7} {:>20} {:>20} {:>20}", i, actual_reward, expected_reward, error);
-//         total_error += error;
-//     }
-
-//     println!("Total error: {}", total_error);
-// }
